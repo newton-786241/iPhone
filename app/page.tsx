fafff4bf6f632f24140242ebf31470a52ea5e305
newton786241 <24tamseelanam11@gmail.com>
2025-01-29 16:24:50 +0530

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Features from "@/Components/Features";
+import Footer from "@/Components/Footer";
 import Hero from "@/Components/Hero";
 import Highlights from "@/Components/Highlights";
+import HowItWorks from "@/Components/HowItWorks";
 import Model from "@/Components/Model";
 import Navbar from "@/Components/Navbar";
 import "@/app/globals.css"
@@ -13,6 +15,8 @@ export default function Home() {
       <Highlights/>
       <Model/>
       <Features/>
+      <HowItWorks/>
+      <Footer/>
     </main>
   );
 }
